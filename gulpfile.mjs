0eb57e758137317cec734855fe4210d0b22bf890
akimaru_OuijaShark <70372038+OuijaShark@users.noreply.github.com>
2025-03-17 18:26:29 +0900

    画像まわり出力修正

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
-const { series, parallel, src, dest, gulpWatch } = gulp;
+const { series, parallel, src, dest } = gulp;
 // 各プラグイン読み込み
+import { deleteAsync } from 'del';
+import rename from 'gulp-rename';
 import imagemin, {mozjpeg, svgo} from 'gulp-imagemin';
 import pngquant from 'imagemin-pngquant';
 import webP from 'gulp-webp';
@@ -8,10 +10,17 @@ import webP from 'gulp-webp';
 // パス設定 ====================================
 const srcPath = {
   img: './src/**/*',
+  imgType: './src/**/*.{png,jpg,jpeg}'
 }
 const destPath = {
   img: './dest/',
 }
+
+// destディレクトリ内のファイルを削除 ============
+const Clean = async () => {
+  await deleteAsync([`${destPath.img}/**`, `!${destPath.img}`]);
+};
+
 // 画像圧縮 ====================================
 const ImgImagemin = () => (
   src(srcPath.img, {encoding: false})
@@ -32,7 +41,7 @@ const ImgImagemin = () => (
             active: false
           }
         ]
-      })
+      }),
     ],{
       verbose: true
     }))
@@ -40,8 +49,20 @@ const ImgImagemin = () => (
 );
 export { ImgImagemin };
 
+// webP変換 ====================================
+const ImgWebp = () => {
+  return src(srcPath.imgType, {encoding: false})
+    .pipe(rename(function (path) {
+      const ext = path.extname; // 元の拡張子（.jpg, .png など）
+      path.basename += ext; // ファイル名に元の拡張子を追加
+      path.extname = '.webp'; // 最終的な拡張子を .webp に変更
+    }))
+    .pipe(webP())
+    .pipe(dest(destPath.img));
+};
+
 // 実行するタスク =============================
 // const defaultTask = series( ImgImagemin );
 
 // 実行用 ====================================
-export default series(ImgImagemin);
+export default series(Clean, ImgImagemin, ImgWebp);
